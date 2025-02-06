02fb08cd09d0a1cc0bda5ec2234a0de731a6cfc8
bigdale123 <dylcal138@gmail.com>
2025-02-05 21:04:50 -0600

    gitignore update, nixos exodus p2

--- Code/User/History/-19472123/YoWW.nix
+++
@@ -1,1 +0,0 @@
-import (fetchTarball "https://github.com/tadfisher/gradle2nix/archive/master.tar.gz")  {}
--- Code/User/History/-214424cc/Thix.nix
+++
@@ -1,7 +0,0 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
-in
-{
-  hello = pkgs.callPackage ./hello.nix { };
-}
